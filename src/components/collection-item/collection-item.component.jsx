aade7ae34f3258b45ed564c8997c8ffc9338fd52
Adebayo <adebayotobiloba9@gmail.com>
2020-12-29 23:32:27 +0100

    added

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,15 +1,17 @@
-import React from 'react';
+import React from "react";
 
-import './collection-item.styles.css'
+import "./collection-item.styles.css";
+import CustomButton from "../custom-button/custom-button.component";
 
-const CollectionItem = ({ id, name, price, imageUrl}) => (
+const CollectionItem = ({ id, name, price, imageUrl }) => (
   <div className="collection-item">
-    <div className="image" style={{backgroundImage: `url(${imageUrl})`}} />
+    <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
     <div className="collection-footer">
       <span className="name">{name}</span>
       <span className="price">{price}</span>
     </div>
+    <CustomButton inverted>ADD TO CART</CustomButton>
   </div>
-)
+);
 
 export default CollectionItem;
