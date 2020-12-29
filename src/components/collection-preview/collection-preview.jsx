ad0d9682df5dd9f6a5534a25053a7355eb371c87
Adebayo <adebayotobiloba9@gmail.com>
2020-12-30 00:39:41 +0100

    added

--- src/components/collection-preview/collection-preview.jsx
+++ src/components/collection-preview/collection-preview.jsx
@@ -1,19 +1,19 @@
-import React from 'react';
+import React from "react";
 
-import './collection-preview.styles.css';
-import CollectionItem from '../collection-item/collection-item.component';
+import "./collection-preview.styles.css";
+import CollectionItem from "../collection-item/collection-item.component";
 
-const CollectionPreview = ({title, items}) => (
-  <div className='collection-preview'>
-    <h1 className='title'>{title.toUpperCase()}</h1>
+const CollectionPreview = ({ title, items }) => (
+  <div className="collection-preview">
+    <h1 className="title">{title.toUpperCase()}</h1>
     <div className="preview">
-      {
-        items.filter((item, idx) => idx < 4).map(({id, ...otherItemProps}) => (
-          <CollectionItem key={id} {...otherItemProps} />
-        ))
-      }
+      {items
+        .filter((item, idx) => idx < 4)
+        .map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
     </div>
   </div>
-)
+);
 
 export default CollectionPreview;
