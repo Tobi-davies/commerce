61d71debc178128ae4096007c0565f0f1d5259da
Adebayo <adebayotobiloba9@gmail.com>
2021-01-03 02:26:17 +0100

    updated redux

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -11,7 +11,11 @@ const CheckoutItem = ({ item, removeItem }) => {
         <img src={imageUrl} alt="img" />
       </div>
       <span className="name">{name}</span>
-      <span className="quantity">{quantity}</span>
+      <span className="quantity">
+        <div className="arrow">&#10094;</div>
+        <span className="value">{quantity}</span>
+        <div className="arrow">&#10095;</div>
+      </span>
       <span className="price">{price}</span>
       <div className="remove-button" onClick={() => removeItem(item)}>
         &#10005;
