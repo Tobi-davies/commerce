1848b9a8d6174ca4d0909ac6b2686daf47c2d973
Adebayo <adebayotobiloba9@gmail.com>
2021-01-04 05:05:41 +0100

    updating redux

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,10 +1,20 @@
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"],
+};
 
 import userReducer from "./user/user.reducer";
 
 import cartReducer from "./cart/cart.reducer";
 
-export default combineReducers({
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);
