6d657bbee3c401aace963f9f7cfd23345def975b
Adebayo <adebayotobiloba9@gmail.com>
2021-01-06 23:19:38 +0100

    fix link

--- src/index.js
+++ src/index.js
@@ -5,14 +5,18 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from "./redux/store";
+import { store, persistor } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
 // import * as serviceWorker from './serviceWorker';
+// import store from "./redux/store";
 
 ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
       <BrowserRouter>
-        <App />
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
       </BrowserRouter>
     </React.StrictMode>
   </Provider>,
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -2,15 +2,15 @@ import { combineReducers } from "redux";
 import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 
+import userReducer from "./user/user.reducer";
+
+import cartReducer from "./cart/cart.reducer";
+
 const persistConfig = {
   key: "root",
   storage,
   whitelist: ["cart"],
 };
-
-import userReducer from "./user/user.reducer";
-
-import cartReducer from "./cart/cart.reducer";
 
 const rootReducer = combineReducers({
   user: userReducer,
