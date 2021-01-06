8c59f586991fcbc559b372c4834fc52a65608a33
Adebayo <adebayotobiloba9@gmail.com>
2021-01-06 23:59:53 +0100

    fix link

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,7 +1,7 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
-import './directory.styles.css';
-import MenuItem from '../menu-item/menu-item.component';
+import "./directory.styles.css";
+import MenuItem from "../menu-item/menu-item.component";
 
 // class Directory extends React.Component {
 //   constructor() {
@@ -56,57 +56,49 @@ import MenuItem from '../menu-item/menu-item.component';
 //   }
 // }
 
-
-
-
-
-
-
-
-
 const Directory = () => {
-  const [sections, updateSections] = useState( [
+  const [sections, updateSections] = useState([
     {
-      title: 'hats',
-      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+      title: "hats",
+      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
       id: 1,
-      linkUrl: 'shop/hats'
+      linkUrl: "shop/hats",
     },
     {
-      title: 'jackets',
-      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+      title: "jackets",
+      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
       id: 2,
-      linkUrl: 'shop/jackets'
+      linkUrl: "shop/jackets",
     },
     {
-      title: 'sneakers',
-      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+      title: "sneakers",
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
       id: 3,
-      linkUrl: 'shop/sneakers'
+      linkUrl: "shop/sneakers",
     },
     {
-      title: 'womens',
-      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-      size: 'large',
+      title: "womens",
+      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+      size: "large",
       id: 4,
-      linkUrl: 'shop/womens'
+      linkUrl: "shop/womens",
     },
     {
-      title: 'mens',
-      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-      size: 'large',
+      title: "mens",
+      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+      size: "large",
       id: 5,
-      linkUrl: 'shop/mens'
-    }
+      linkUrl: "shop/mens",
+    },
   ]);
 
-  return(
+  return (
     <div className="directory-menu">
-      {sections.map(({ title, imageUrl, id, size }) => (
-        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+      {sections.map(({ id, ...otherSectionProps }) => (
+        <MenuItem key={id} {...otherSectionProps} />
       ))}
     </div>
-  )
-}
+  );
+};
 
 export default Directory;
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,17 +1,23 @@
-import React from 'react';
-import { withRouter } from 'react-router-dom';
-import './menu-item.styles.css';
+import React from "react";
+import { withRouter } from "react-router-dom";
+import "./menu-item.styles.css";
 
-const MenuItem = ({ title, imageUrl, size, history }) => {
-  return(
-    <div className={`${size} menu-item`} onClick={() => history.push()}>
-      <div className='background-image' style={{backgroundImage: `url(${imageUrl})`}} />
+const MenuItem = ({ title, imageUrl, size, linkUrl, history, match }) => {
+  return (
+    <div
+      className={`${size} menu-item`}
+      onClick={() => history.push(`${match.url}${linkUrl}`)}
+    >
+      <div
+        className="background-image"
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      />
       <div className="content">
         <h1 className="title">{title.toUpperCase()}</h1>
         <span className="subtitle">SHOP NOW</span>
       </div>
     </div>
-  )
+  );
 };
 
 export default withRouter(MenuItem);
