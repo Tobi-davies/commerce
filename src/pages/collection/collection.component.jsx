ba0ce2544310e69508a8a646755d754ccb3321ea
Adebayo <adebayotobiloba9@gmail.com>
2021-01-07 11:35:28 +0100

    redux

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import CollectionItem from "../../components/collection-item/collection-item.component";
+import { connect } from "react-redux";
+import { selectCollection } from "../../redux/shop/shop.selectors";
 
-const CollectionPage = () => (
-  <div className="collection-page">
-    <CollectionItem />
-  </div>
-);
+const CollectionPage = ({ collection }) => {
+  console.log(collection);
 
-export default CollectionPage;
+  return (
+    <div className="collection-page">
+      <h1>hello</h1>
+    </div>
+  );
+};
+
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state),
+});
+
+export default connect(mapStateToProps)(CollectionPage);
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
 import { Route } from "react-router-dom";
-import CollectionItem from "../../components/collection-item/collection-item.component";
+import CollectionPage from "../collection/collection.component";
 
 // import SHOP_DATA from "./shop.data";
 // import { useState } from "react";
@@ -13,7 +13,11 @@ const ShopPage = ({ match }) => {
   return (
     <div className="shop-page">
       <Route exact path={`${match.url}`} component={CollectionsOverview} />
-      <Route exact path={`${match.url}`} component={CollectionItem} />
+      <Route
+        exact
+        path={`${match.url}/:collectionId`}
+        component={CollectionPage}
+      />
     </div>
   );
 };
