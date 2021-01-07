a27ed8d845b487f113ff5ecbe5d63a0f40877c83
Adebayo <adebayotobiloba9@gmail.com>
2021-01-07 10:58:48 +0100

    redux

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import CollectionItem from "../../components/collection-item/collection-item.component";
+
+const CollectionPage = () => (
+  <div className="collection-page">
+    <CollectionItem />
+  </div>
+);
+
+export default CollectionPage;
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
 import { Route } from "react-router-dom";
+import CollectionItem from "../../components/collection-item/collection-item.component";
 
 // import SHOP_DATA from "./shop.data";
 // import { useState } from "react";
@@ -12,6 +13,7 @@ const ShopPage = ({ match }) => {
   return (
     <div className="shop-page">
       <Route exact path={`${match.url}`} component={CollectionsOverview} />
+      <Route exact path={`${match.url}`} component={CollectionItem} />
     </div>
   );
 };
