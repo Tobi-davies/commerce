59efc61c1e44c0b06c02f6b6ada7f7f90e6b742e
Adebayo <adebayotobiloba9@gmail.com>
2021-01-07 20:46:23 +0100

    updating stripe integration

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+import StripeCheckout from "react-stripe-checkout";
+
+const StriptCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51I73R8FwU1ktTra1oewqm3qXTny3lpbqKbViydrJgfJyVR3fn1CajWrNJZDKYczo6hIIVzsknWUoLXNNxNQqyt9I00M1qXfLFv";
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="Davies CROWN Clothing"
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
