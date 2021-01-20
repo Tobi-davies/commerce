18719246aaa3e4ec3699bda2d6adaccfbf559551
Adebayo <adebayotobiloba9@gmail.com>
2021-01-21 00:47:55 +0100

    added spinner

--- src/components/collection-preview/collection-preview.jsx
+++ src/components/collection-preview/collection-preview.jsx
@@ -1,19 +1,29 @@
 import React from "react";
+import { withRouter } from "react-router-dom";
 
 import "./collection-preview.styles.css";
 import CollectionItem from "../collection-item/collection-item.component";
 
-const CollectionPreview = ({ title, items }) => (
-  <div className="collection-preview">
-    <h1 className="title">{title.toUpperCase()}</h1>
-    <div className="preview">
-      {items
-        .filter((item, idx) => idx < 4)
-        .map((item) => (
-          <CollectionItem key={item.id} item={item} />
-        ))}
-    </div>
-  </div>
-);
+const CollectionPreview = ({ title, items, history, match, routeName }) => {
+  console.log(match);
 
-export default CollectionPreview;
+  return (
+    <div className="collection-preview">
+      <h1
+        className="title"
+        onClick={() => history.push(`${match.path}/${routeName}`)}
+      >
+        {title.toUpperCase()}
+      </h1>
+      <div className="preview">
+        {items
+          .filter((item, idx) => idx < 4)
+          .map((item) => (
+            <CollectionItem key={item.id} item={item} />
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default withRouter(CollectionPreview);
