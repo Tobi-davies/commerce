bb2152424233dd05eb1fbf95d13683135565f7b3
Adebayo <adebayotobiloba9@gmail.com>
2021-01-20 09:06:17 +0100

    updated firestore

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -3,6 +3,10 @@ import CollectionsOverview from "../../components/collections-overview/collectio
 import { Route } from "react-router-dom";
 import CollectionPage from "../collection/collection.component";
 
+import { firestore } from "../../firebase/firebase.utils";
+import { useEffect } from "react";
+// import { render } from "node-sass";
+
 // import SHOP_DATA from "./shop.data";
 // import { useState } from "react";
 
@@ -10,6 +14,15 @@ import CollectionPage from "../collection/collection.component";
 // import { selectCollections } from "../../redux/shop/shop.selectors";
 
 const ShopPage = ({ match }) => {
+  // unsubscribeFromSnapshot = null;
+
+  useEffect(() => {
+    const collectionRef = firestore.collection("collections");
+    collectionRef.onSnapshot(async (snapshot) => {
+      console.log(snapshot);
+    });
+  });
+
   return (
     <div className="shop-page">
       <Route exact path={`${match.url}`} component={CollectionsOverview} />
@@ -23,3 +36,29 @@ const ShopPage = ({ match }) => {
 };
 
 export default ShopPage;
+
+// class ShopPage extends React.Component {
+//   unsubscribeFromSnapshot = null;
+
+//   componentDidMount() {
+//     const collectionRef = firestore.collection("collections");
+//     collectionRef.onSnapshot(async (snapshot) => {
+//       console.log(snapshot);
+//     });
+//   }
+
+//   render() {
+//     const { match } = this.props;
+
+//     return (
+//       <div className="shop-page">
+//         <Route exact path={`${match.url}`} component={CollectionsOverview} />
+//         <Route
+//           exact
+//           path={`${match.url}/:collectionId`}
+//           component={CollectionPage}
+//         />
+//       </div>
+//     );
+//   }
+// }
