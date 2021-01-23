d2bccf4b8c8894d7be1e3505019001baeee1f6c0
Adebayo <adebayotobiloba9@gmail.com>
2021-01-23 08:30:07 +0100

    implementing redux thunk

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,10 @@
 import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import { persistStore } from "redux-persist";
+import thunk from "redux-thunk";
 import rootReducer from "./root-reducer";
 
-const middlewares = [logger];
+const middlewares = [thunk, logger];
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
