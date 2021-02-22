17a356d5aa1e407ba9e5258b2f8f324fa133c481
Adebayo <adebayotobiloba9@gmail.com>
2021-02-23 00:18:57 +0100

    implemented redux saga

--- src/App.js
+++ src/App.js
@@ -87,4 +87,91 @@ const mapDispatchToProps = (dispatch) => ({
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
 
-// export default App;
+// import React, { useEffect } from "react";
+// // import logo from './logo.svg';
+// import "./App.css";
+// import HomePage from "./pages/homepage/homepage.component";
+// import { Route, Switch, Redirect } from "react-router-dom";
+// import ShopPage from "./pages/shop/shop.component";
+// import Header from "./components/header/header.component";
+// import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-sign-up.component";
+// import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
+// import { setCurrentUser } from "./redux/user/user.actions";
+// import { connect } from "react-redux";
+// import { selectCurrentUser } from "./redux/user/user.selectors";
+// import { createStructuredSelector } from "reselect";
+// import CheckoutPage from "./components/checkout/checkout.component";
+
+// const App = (props) => {
+//   // const [currentUser, updateCurrentUser] = useState(null);
+//   // const changeName = () => {
+//   //   setName("Tobenz")
+//   // }
+//   // console.log(currentUser);
+
+//   // const unsubscribeFromAuth = null
+
+//   useEffect(() => {
+//     const unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+//       // createUserProfileDocument(user);
+
+//       if (userAuth) {
+//         const userRef = await createUserProfileDocument(userAuth);
+
+//         userRef.onSnapshot((snapShot) => {
+//           // updateCurrentUser({
+//           //   id: snapShot.id,
+//           //   ...snapShot.data(),
+//           // });
+//           props.setCurrentUser({
+//             id: snapShot.id,
+//             ...snapShot.data(),
+//           });
+
+//           // console.log(currentUser);
+//         });
+//         // console.log(currentUser);
+//       } else {
+//         // updateCurrentUser(userAuth);
+//         props.setCurrentUser(userAuth);
+//       }
+//     });
+
+//     return () => {
+//       unsubscribeFromAuth();
+//     };
+//     // eslint-disable-next-line
+//   }, []);
+
+//   return (
+//     <div className="App">
+//       <Header />
+//       <Switch>
+//         <Route exact path="/" component={HomePage} />
+//         <Route path="/shop" component={ShopPage} />
+//         <Route exact path="/checkout" component={CheckoutPage} />
+//         <Route
+//           exact
+//           path="/signin"
+//           render={() =>
+//             props.currentUser ? <Redirect to="/" /> : <SignInAndSignUpPage />
+//           }
+//         />
+//       </Switch>
+//     </div>
+//   );
+// };
+
+// // const mapStateToProps = ({ user }) => ({
+// //   currentUser: user.currentUser,
+// // });
+
+// const mapStateToProps = createStructuredSelector({
+//   currentUser: selectCurrentUser,
+// });
+
+// const mapDispatchToProps = (dispatch) => ({
+//   setCurrentUser: (user) => dispatch(setCurrentUser(user)),
+// });
+
+// export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,6 +1,35 @@
 import UserActionTypes from "./user.types";
+import userActionTypes from "./user.types";
 
 export const setCurrentUser = (user) => ({
   type: UserActionTypes.SET_CURRENT_USER,
   payload: user,
+});
+
+export const googleSignInStart = () => ({
+  type: UserActionTypes.GOOGLE_SIGN_IN_START,
+});
+
+export const googleSignInSuccess = (user) => ({
+  type: userActionTypes.GOOGLE_SIGN_IN_SUCCESS,
+  payload: user,
+});
+
+export const googleSignInFailure = (error) => ({
+  type: userActionTypes.GOOGLE_SIGN_IN_FAILURE,
+  payload: error,
+});
+export const emailSignInStart = (emailAndPassword) => ({
+  type: UserActionTypes.EMAIL_SIGN_IN_START,
+  password: emailAndPassword,
+});
+
+export const emailSignInSuccess = (user) => ({
+  type: userActionTypes.EMAIL_SIGN_IN_SUCCESS,
+  payload: user,
+});
+
+export const emailSignInFailure = (error) => ({
+  type: userActionTypes.EMAIL_SIGN_IN_FAILURE,
+  payload: error,
 });
