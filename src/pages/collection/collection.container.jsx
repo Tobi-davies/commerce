b1ec8ac0b461937f73b543ead91d9bf7569a8a93
Adebayo <adebayotobiloba9@gmail.com>
2021-02-22 15:59:30 +0100

    implemented shop data

--- src/pages/collection/collection.container.jsx
+++ src/pages/collection/collection.container.jsx
@@ -7,7 +7,7 @@ import WithSpinner from "../../components/with-spinner/with-spinner.component";
 import CollectionPage from "./collection.component";
 
 const mapStateToProps = createStructuredSelector({
-  isLoading: (state) => selectIsCollectionLoaded(state),
+  isLoading: (state) => !selectIsCollectionLoaded(state),
 });
 
 const CollectionPageContainer = compose(
