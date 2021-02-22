02b4e31325feb6d6fc9f6c056d8fae8308b2d96b
Adebayo <adebayotobiloba9@gmail.com>
2021-02-22 22:59:41 +0100

    implemented redux saga

--- src/redux/shop/shop.saga.js
+++ src/redux/shop/shop.saga.js
@@ -1,4 +1,4 @@
-import { takeEvery, call, put } from "redux-saga/effects";
+import { takeLatest, call, put } from "redux-saga/effects";
 import ShopActionTypes from "./shop.types";
 import {
   firestore,
@@ -26,7 +26,7 @@ export function* fetchCollectionAsync() {
 }
 
 export function* fetchCollectionStart() {
-  yield takeEvery(
+  yield takeLatest(
     ShopActionTypes.FETCH_COLLECTIONS_START,
     fetchCollectionAsync
   );
